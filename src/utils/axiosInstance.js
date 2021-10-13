56a94ce61867266aa60ab11a33b4d28fac08b53c
Madhamba <u201621181@upc.edu.pe>
2021-10-12 22:23:56 -0500

    - feat: deployment

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5000/"
+    baseURL: "https://monicovid-19.herokuapp.com"
 });
 
 axiosInstance.interceptors.request.use((config) => {
